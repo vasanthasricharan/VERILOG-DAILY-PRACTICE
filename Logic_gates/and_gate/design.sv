79ce1d8e053cddb301d8b2bc09df14687bf4880e
Sri_Charan <154444424+vasanthasricharan@users.noreply.github.com>
2025-06-24 17:07:11 +0530

    design.sv

--- Logic_gates/and_gate/design.sv
+++ Logic_gates/and_gate/design.sv
@@ -1,12 +1,9 @@
-module logic_gates(a,b,and_out,or_out,xor_out,nand_out,nor_out,xnor_out,not_out);
-  	input a,b;
-  	output and_out, or_out, xor_out, nand_out, nor_out, xnor_out, not_out;
-  
- 	assign and_out = a & b;
- 	assign or_out = a | b;
-    assign xor_out = a ^ b;
- 	assign nand_out = ~(a & b);
- 	assign nor_out = ~(a | b);
-    assign xnor_out = ~(a ^ b);
-    assign not_out = ~a;
+module and_gate(
+	input wire a,
+	input wire b,
+	output wire y
+);
+
+assign y = a & b;
+
 endmodule
